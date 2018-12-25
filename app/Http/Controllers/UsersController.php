083ee1ea0010f2c8b7fffabc5a86ee7c417409e0
Jiang <237132868@qq.com>
2018-12-25 09:15:38 +0000

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -34,4 +34,25 @@ class UsersController extends Controller
         session()->flash('success', '欢迎,您将在这里开始一段新的旅程');
         return redirect()->route('users.show', [$user]);
     }
+
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|min:6|confirmed'
+        ]);
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = $request->password;
+        }
+        $user->update($data);
+        session()->flash('success', '个人资料更新成功');
+        return redirect()->route('users.show',$user);
+    }
 }
