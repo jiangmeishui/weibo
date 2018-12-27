ae579204a5fed558dc283d603e1fee2e8b708a9f
Jiang <237132868@qq.com>
2018-12-27 07:53:47 +0000

    移除邮件发送form选项

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -76,12 +76,10 @@ class UsersController extends Controller
     protected function sendEmailConfirmationTo($user) {
         $view = 'emails.confirm';
         $data = compact('user');
-        $from = '237132868@qq.com';
-        $name = 'jiang';
         $to = $user->email;
         $subject = '感谢注册web应用! 请确认您的邮箱';
-        Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
-            $message->from($from, $name)->to($to)->subject($subject);
+        Mail::send($view, $data, function ($message) use ($to, $subject) {
+            $message->to($to)->subject($subject);
         });
     }
 
